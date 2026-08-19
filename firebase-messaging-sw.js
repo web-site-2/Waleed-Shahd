cc1c866388ce1ae53e27510d14208a49e262dce3
web-site-2 <gplus2849@gmail.com>
2026-08-19 21:27:45 +0300

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js');
+
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBhw9-b7a3NZJXG8jxEfVC8ZMw44eMc27A",
+  authDomain: "new-1-6d173.firebaseapp.com",
+  projectId: "new-1-6d173",
+  storageBucket: "new-1-6d173.firebasestorage.app",
+  messagingSenderId: "102791475836",
+  appId: "1:102791475836:web:1365479a319eeed9514ff9"
+});
+
+const messaging = firebase.messaging();
